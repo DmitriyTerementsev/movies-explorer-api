a17abaa29eba1c4e6e90446da735b5ab33585d34
Alchimik981 <d.terementsev@yandex.ru>
2023-12-12 02:00:17 +0300

    routes

--- routes/movies.js
+++ routes/movies.js
@@ -1,15 +1,17 @@
-const routerMovies = require('express').Router();
+const routerMovies = require("express").Router();
 const {
-  getMovies, createMovie, deleteMovieById, likeMovie, dislikeMovie,
-} = require('../controllers/movie');
-const {
-  validateMovie, validateMovieId,
-} = require('../validator/validator');
+  getMovies,
+  createMovie,
+  deleteMovieById,
+  likeMovie,
+  dislikeMovie,
+} = require("../controllers/movie");
+const { validateMovie } = require("../validator/validator");
 
-routerMovies.get('/', getMovies);
-routerMovies.post('/', validateMovie, createMovie);
-routerMovies.delete('/:movieId', validateMovieId, deleteMovieById);
-routerMovies.put('/:movieId/likes', validateMovieId, likeMovie);
-routerMovies.delete('/:movieId/likes', validateMovieId, dislikeMovie);
+routerMovies.get("/", getMovies);
+routerMovies.post("/", validateMovie, createMovie);
+routerMovies.delete("/:movieId", deleteMovieById);
+routerMovies.put("/:movieId/likes", likeMovie);
+routerMovies.delete("/:movieId/likes", dislikeMovie);
 
 module.exports = routerMovies;
