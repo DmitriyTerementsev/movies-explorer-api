8aba106319224f080ac4277f83c40f7c753d7c53
Alchimik981 <d.terementsev@yandex.ru>
2023-12-12 01:58:49 +0300

    fix routes

--- routes/movies.js
+++ routes/movies.js
@@ -8,8 +8,8 @@ const {
 
 routerMovies.get('/', getMovies);
 routerMovies.post('/', validateMovie, createMovie);
-routerMovies.delete('/_id', validateMovieId, deleteMovieById);
-routerMovies.put('/_id/likes', validateMovieId, likeMovie);
-routerMovies.delete('/_id/likes', validateMovieId, dislikeMovie);
+routerMovies.delete('/:movieId', validateMovieId, deleteMovieById);
+routerMovies.put('/:movieId/likes', validateMovieId, likeMovie);
+routerMovies.delete('/:movieId/likes', validateMovieId, dislikeMovie);
 
 module.exports = routerMovies;
